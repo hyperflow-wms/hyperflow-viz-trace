a90b451b91eedda10a62da3d144ad672628d58c4
Andrzej Broński <andrzej1_1@o2.pl>
2020-11-07 21:24:03 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="hyperflow-viz-trace",
+    version="0.1.0",
+    description="Tool for visualization of HyperFlow execution traces",
+    url="https://github.com/hyperflow-wms/hyperflow-viz-trace",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'hflow-viz-trace = hyperflow_viz_trace.main:main',
+        ],
+    },
+    install_requires=[
+        'seaborn',
+        'jsonlines',
+        'pandas',
+        'natsort',
+        'scipy',
+    ]
+)

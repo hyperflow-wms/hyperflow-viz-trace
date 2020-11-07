9a8c5d5915c2d3143f81fc2f529efaa051a3117d
Bartosz Balis <balis@agh.edu.pl>
2020-11-07 18:52:22 +0100

    Remove old installation commands

--- bin.sh
+++ bin.sh
@@ -1,20 +1,3 @@
 #!/usr/bin/env bash
 set -e
-
-PACKAGE="hflow-tools"
-NPM_ROOT=$(npm root -g)
-
-#pushd . > /dev/null
-#cd ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace
-
-#PYTHON="$(pipenv --venv)/bin/python"
-#if [ ! -f $PYTHON ]; then
-#  # fallback to windows path
-#  PYTHON="$(pipenv --venv)/Scripts/python.exe"
-#fi
-
-#popd  > /dev/null
-
-#$PYTHON ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace/main.py $@
-
-python ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace/main.py $@
+python /hyperflow-viz-trace/main.py $@
